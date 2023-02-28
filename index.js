809e21d68c54b5337e46fb82122166ef7af55a04
sebaGonzalez <seba.gonzalez84@gmail.com>
2023-02-28 03:06:06 -0300

    Initial commit
    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function changeTitle() {
+    document.getElementById("titulo").style.backgroundColor = "#808080"
+}
+
+function restoreTitle() {
+    document.getElementById("titulo").style.backgroundColor = "#FFFFFF"
+}
